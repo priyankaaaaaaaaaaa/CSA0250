c1eafe7a08eb8da1e9f6ccd8b03efa9edc1d64a6
priyankaaaaaaaaaaa <128682849+priyankaaaaaaaaaaa@users.noreply.github.com>
2023-03-23 17:58:05 +0530

    Add files via upload

---
+++ strcat.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char c;
+	printf("enter a character: ");
+	scanf("%c", &c);
+	
+	printf("ASCII value of  %c = %d",c,c);
+	
+	return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+
