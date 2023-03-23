634d46a5f9a2b4218f385ac7c43a299f4c47bca1
priyankaaaaaaaaaaa <128682849+priyankaaaaaaaaaaa@users.noreply.github.com>
2023-03-23 17:42:13 +0530

    Add files via upload

---
+++ diagonal.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+
+{
+	int array1 [10][10],i,j,m,n,sum=0;
+	
+	printf("enter no.of rows :: ");
+	scanf("%d",&m);
+	printf("\nEnter no.of cols :: ");
+	scanf("%d", &n);
+	printf("\nEnter values of the matrix ::\n");
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("\nEnter a[%d][%d] value :: ",i,j);
+			scanf("%d",&array1[i][j]);
+		}
+			
+}
+printf("\nThe Diagonal elements of a matrix are :: \n\n");
+if(m==n)
+{
+	for (i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i==j)
+			   printf("\t%d",array1 [i][j]);
+			else
+			    printf("\t");
+		}
+		printf("\n\n");
+	}
+}
+else
+{
+	printf("\nMatrix is not a square matrix.");
+}
+return 0;
+}
+	
+		
+	
+	
